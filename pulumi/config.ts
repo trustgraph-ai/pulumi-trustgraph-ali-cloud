2d7bd14664605c8e56f092e5236da3ef15902386
Cyber MacGeddon <cybermaggedon@gmail.com>
2026-06-03 16:47:32 +0100

    Tweak node size

--- pulumi/config.ts
+++ pulumi/config.ts
@@ -30,6 +30,6 @@ export const tagsSep = Object.entries(tags).map(
 
 export const prefix = "trustgraph-" + environment;
 
-export const nodeType = "ecs.g6.xlarge";
+export const nodeType = "ecs.g7.xlarge";
 export const nodeCount = 3;
 export const diskSize = 40;
